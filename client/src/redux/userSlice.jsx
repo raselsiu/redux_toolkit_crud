6b357ef97a03a1f2d3035fd134b3f3361724b7a7
raselsiu <raselqax@gmail.com>
2023-11-06 01:24:06 +0600

    reduxtoolkit

---
+++ client/src/redux/userSlice.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: {
+    users: [],
+  },
+  reducers: {},
+});
+
+export const {} = userSlice.actions;
+export default userSlice.reducer;
